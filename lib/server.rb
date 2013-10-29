7360e5624a69fa52f47d2c3e37f92e7af53894b8
Katrina Engelsted <katrina@engelsted.co>
2013-10-29 15:15:09 -0600

    ended test

--- lib/server.rb
+++ lib/server.rb
@@ -8,12 +8,35 @@ class BikeDepot < Sinatra::Base
     register Sinatra::Reloader
   end
 
+  helpers do
+  def protected!
+    return if authorized?
+    headers['WWW-Authenticate'] = 'Basic realm="Restricted Area"'
+    halt 401, "Not authorized\n"
+  end
+
+  def authorized?
+    @auth ||=  Rack::Auth::Basic::Request.new(request.env)
+    @auth.provided? and @auth.basic? and @auth.credentials and @auth.credentials == ['admin', 'admin']
+  end
+  end
+
   not_found do
     erb :error
   end
 
+  get '/' do
+    "Everybody can see this page"
+  end
+
+  get '/protected' do
+    protected!
+    "Welcome, authenticated client"
+  end
+
+
   get '/services' do
-    
+
   end
 
   get '/' do
@@ -21,7 +44,6 @@ class BikeDepot < Sinatra::Base
   end
 
   get '/service_types' do 
-    
-  end
 
+  end
 end
---
+++ test/acceptance/auth_test.rb
@@ -0,0 +1,27 @@
+require_relative '../helpers/acceptance_helper'
+
+class BikeDepotTest < Minitest::Test
+  include Rack::Test::Methods
+
+   def app
+    BikeDepot
+  end
+
+  def test_without_authentication
+    get '/protected'
+    assert_equal 401, last_response.status
+  end
+
+  def test_with_bad_credentials
+    authorize 'bad', 'boy'
+    get '/protected'
+    assert_equal 401, last_response.status
+  end
+
+  def test_with_proper_credentials
+    authorize 'admin', 'admin'
+    get '/protected'
+    assert_equal 200, last_response.status
+    assert_equal "Welcome, authenticated client", last_response.body
+  end
+end
