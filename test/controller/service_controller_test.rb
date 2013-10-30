2e00c9000953c3e9395b462c5c758c6e2f83f8c7
Simon Taranto <simon.taranto@gmail.com>
2013-10-30 13:18:42 -0600

    add service_tpye to test data

--- test/controller/service_controller_test.rb
+++ test/controller/service_controller_test.rb
@@ -8,12 +8,20 @@ require './lib/server'
 class ServiceControllerTest < Minitest::Test
   include Rack::Test::Methods
 
+  attr_reader :type_id
+
   def app
     BikeDepot
   end
 
+  def setup
+    ServiceTypes.save(ServiceType.new(:name => "Wheels"))
+    @type_id = ServiceTypes.find_all_by_name("Wheels").first.id
+  end
+
   def test_services_page_works
-    Services.save(Service.new(:name => "Rotation"))
+    Services.save(Service.new(:name => "Rotation",
+                              :service_type_id => type_id))
     get '/services'
     assert last_response.ok?, "getting '/services' should work"
     assert last_response.body.include?("Rotation"), "should include Rotation"
@@ -21,12 +29,10 @@ class ServiceControllerTest < Minitest::Test
   end
 
   def test_it_posts_a_service
-    post '/services', :service => {:name => "fix wheel"} #need to add service_type_id
+    post '/services', :service => {:name => "fix wheel", :service_type_id => type_id} #need to add service_type_id
     follow_redirect!
     assert last_response.ok?, "should work with new service"
     assert last_response.body.include?("fix wheel"), "should include fix wheel"
   end
-
-
 
 end
