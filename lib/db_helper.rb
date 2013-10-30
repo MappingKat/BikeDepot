fc411c423e2577f05c8c49bb2aa5d2ea14e4ad02
Simon Taranto <simon.taranto@gmail.com>
2013-10-30 13:55:11 -0600

    update db_helper to dynamically call db name

--- lib/db_helper.rb
+++ lib/db_helper.rb
@@ -12,14 +12,23 @@ module Db_helper
     end
   end
 
+  def self.database_connection
+    if ENV['RACK_ENV'] == 'production'
+      @db ||= Sequel.connect(ENV['DATABASE_URL'])
+      return @db
+    else
+      return @db
+    end
+  end
+
   def self.run_migrations
-    unless @db.table_exists?(:service_types)
-      @db.run "CREATE TABLE service_types (id INTEGER PRIMARY KEY AUTOINCREMENT,
+    unless database_connection.table_exists?(:service_types)
+      database_connection.run "CREATE TABLE service_types (id INTEGER PRIMARY KEY AUTOINCREMENT,
                                              name VARCHAR(255))"
     end
 
-    unless @db.table_exists?(:services)
-      @db.run "CREATE TABLE services (id INTEGER PRIMARY KEY AUTOINCREMENT,
+    unless database_connection.table_exists?(:services)
+      database_connection.run "CREATE TABLE services (id INTEGER PRIMARY KEY AUTOINCREMENT,
                                       service_type_id INTEGER,
                                       name VARCHAR(255),
                                       description VARCHAR(255),
