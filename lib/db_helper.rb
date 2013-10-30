c41ebe4644333bd66d33625180f8ce06b89d90a6
Simon Taranto <simon.taranto@gmail.com>
2013-10-30 14:02:58 -0600

    update @db var definition

--- lib/db_helper.rb
+++ lib/db_helper.rb
@@ -1,8 +1,8 @@
 module Db_helper
 
   def self.database
-    unless ENV['RACK_ENV'] == 'test'
-      @db ||= ENV['DATABASE_URL'] || Sequel.sqlite('database.sqlite3')
+    if !ENV['RACK_ENV'] == 'test'
+      @db ||= ENV['DATABASE_URL'] ? Sequel.connect(ENV['DATABASE_URL']) : Sequel.sqlite('database.sqlite3')
       run_migrations
       return @db
     else
@@ -12,23 +12,14 @@ module Db_helper
     end
   end
 
-  def self.database_connection
-    if ENV['RACK_ENV'] == 'production'
-      @db ||= Sequel.connect(ENV['DATABASE_URL'])
-      return @db
-    else
-      return @db
-    end
-  end
-
   def self.run_migrations
-    unless database_connection.table_exists?(:service_types)
-      database_connection.run "CREATE TABLE service_types (id INTEGER PRIMARY KEY AUTOINCREMENT,
+    unless @db.table_exists?(:service_types)
+      @db.run "CREATE TABLE service_types (id INTEGER PRIMARY KEY AUTOINCREMENT,
                                              name VARCHAR(255))"
     end
 
-    unless database_connection.table_exists?(:services)
-      database_connection.run "CREATE TABLE services (id INTEGER PRIMARY KEY AUTOINCREMENT,
+    unless @db.table_exists?(:services)
+      @db.run "CREATE TABLE services (id INTEGER PRIMARY KEY AUTOINCREMENT,
                                       service_type_id INTEGER,
                                       name VARCHAR(255),
                                       description VARCHAR(255),
